ab1e35cc565edb33e858103c62b0050bdeced26b
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-24 14:56:52 +0000

    radiobuttons
    radio buttons binded to the same model will make only one option selectable.

--- src/App.vue
+++ src/App.vue
@@ -73,13 +73,16 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"
+                                > Female
                     </label>
                 </div>
             </div>
@@ -121,7 +124,7 @@
                               {{item}}
                             </li>
                         </ul>
-                        <p>Gender:</p>
+                        <p>Gender:{{gender}}</p>
                         <p>Priority:</p>
                         <p>Switched:</p>
                     </div>
@@ -141,7 +144,8 @@ export default {
         age:99
       },
       message:'new message',
-      sendMail:[]
+      sendMail:[],
+      gender:'Male'
     }
   }
 }
